ff3224c8e69aba188ae851db250a6db6194843a5
Adilet K <adiletkdev@gmail.com>
2023-07-07 20:06:52 +0600

    spring initializr

---
+++ base-domains/src/test/java/net/adiletkdev/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.adiletkdev.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

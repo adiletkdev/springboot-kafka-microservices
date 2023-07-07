26e83d4a4c4896b3902a3734f71ecb603e977aa7
Adilet K <adiletkdev@gmail.com>
2023-07-07 21:29:51 +0600

    add: OrderService Microservice - create REST API to send Order

---
+++ order-service/src/main/java/net/adiletkdev/orderservice/controller/OrderController.java
@@ -0,0 +1,37 @@
+package net.adiletkdev.orderservice.controller;
+
+import net.adiletkdev.basedomains.dto.Order;
+import net.adiletkdev.basedomains.dto.OrderEvent;
+import net.adiletkdev.orderservice.kafka.OrderProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/")
+public class OrderController {
+
+    private OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("orders")
+    public ResponseEntity<String> placeOrder(@RequestBody Order order) {
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("Order status is in pending state");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+
+        return ResponseEntity.ok("Order placed successfully");
+    }
+}

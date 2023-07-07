e4468cd70fa32e33a18441b9cb1db88a31fbbd23
Adilet K <adiletkdev@gmail.com>
2023-07-07 20:16:20 +0600

    add: Base-Domains microservice - create DTO classes - Order and OrderEvent

---
+++ base-domains/src/main/java/net/adiletkdev/basedomains/dto/Order.java
@@ -0,0 +1,15 @@
+package net.adiletkdev.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private String orderId;
+    private String orderName;
+    private int qty;
+    private double price;
+}
